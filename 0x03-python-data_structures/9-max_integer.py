e2ccbd61e06b710dd2f7abff2f99d49fa494939a
Leboxgang <thevirusbruh@gmail.com>
2024-09-16 07:24:51 -0700

    Test 5

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        return max(my_list)
-    return None
+    if not my_list:
+        return None
+
+    largest = my_list[0]
+
+    for num in my_list:
+        if num > largest:
+            largest = num
+
+    return largest
