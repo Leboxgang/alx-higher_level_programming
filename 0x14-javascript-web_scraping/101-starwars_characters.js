a94583af0f940e886cffb60e0545cfe10bc600a8
Leboxgang <thevirusbruh@gmail.com>
2025-02-11 06:42:52 -0800

    java starwars characters

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  try {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    const fetchCharacter = (index) => {
+      if (index >= characters.length) return;
+
+      request(characters[index], function (err, response, body) {
+        if (!err) {
+          try {
+            console.log(JSON.parse(body).name);
+            fetchCharacter(index + 1);
+          } catch (parseError) {
+            console.error('Error parsing character data:', parseError);
+          }
+        } else {
+          console.error('Error fetching character:', err);
+        }
+      });
+    };
+
+    fetchCharacter(0);
+  } catch (parseError) {
+    console.error('Error parsing movie data:', parseError);
+  }
+});
