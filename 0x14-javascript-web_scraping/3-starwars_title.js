9cca6a1c697a306f3acd4e37c9dba487eb158df0
Leboxgang <thevirusbruh@gmail.com>
2025-02-11 06:27:04 -0800

    java starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
-request(url, function (err, httpResponse, body) {
-  console.log(JSON.parse(body).title || err);
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  try {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
 });
